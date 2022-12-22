1cd598deea98a41459e6e030b4c1c23a3da8cb62
piyush <93872196+piy1577@users.noreply.github.com>
2022-12-22 22:32:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = document.getElementById(title).value;
+        this.author = document.getElementById(author).value;
+        this.isbn = document.getElementById(isbn).value;
+    }
+};
+
